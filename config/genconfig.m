473cec6515775a4d727bd303c75785d218234132
textremo <wfwfpwefe2323@gmail.com>
2025-11-17 17:00:04 +0800

    config: genconfig finished

--- config/genconfig.m
+++ config/genconfig.m
@@ -9,13 +9,30 @@ function genconfig(filename, sheetname, casename)
     sheet_names = sheetnames(filename);
     sheet_num = length(sheet_names);
     
+    
+    % opts = setvartype(opts, 'YourColumnName', 'string');
+
     is_sheet = false;
+    is_case = false;
 
     for i = 1:sheet_num
         sheet_name = sheet_names(i);
         if sheet_name == sheetname
             is_sheet = true;
-            sheet_content = readtable(filename, 'Sheet', i, "VariableNamingRule", 'preserve', 'Range', 'A1:Z100');
+            opts = detectImportOptions(filename, 'Sheet', i);
+            opts.VariableTypes(:) = {'string'};
+            opts.VariableNamingRule = 'preserve';
+            sheet_content = readtable(filename, opts);
+            if ismember(casename, sheet_content.Properties.VariableNames)
+                is_case = true;
+                props_name = sheet_content.(1);
+                props = sheet_content.(casename);
+                props_len = length(props);
+                
+                for prop_id = 1:props_len
+                    assignin('base', props_name(prop_id), eval(props(prop_id)));
+                end
+            end
             break;
         end
 
@@ -24,5 +41,8 @@ function genconfig(filename, sheetname, casename)
 
     if ~is_sheet
         error("[%s] does not exist!!!", sheetname);
+    end
+    if ~is_case
+        error("[%s] does not exist!!!", casename);
     end
 end
